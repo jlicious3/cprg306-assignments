8307202acfc5a3f702a50c16aff8385827f404a0
JJ Marquez <jjmarquez.mnl@gmail.com>
2024-01-29 19:48:10 -0700

    commit

--- app/page.js
+++ app/page.js
@@ -1,21 +1,20 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from 'next/image';
+import Link from 'next/link';
 import { FaApple } from 'react-icons/fa';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-4x1 text-red-900 border-2 border-red-800 p-10">
-        The chicken is yummy?
+      <div className="text-4x1 text-black-900 border-4 border-red-800 p-5">
+        <h1>CPRG 306: Web Development 2 - Assignments</h1>
       </div>
-      <div>
-        Click the apol
-      </div>
-      <div className="text-5x1 text-grey-900 border-0 items-center">
-        <Link href="/apples">
+      
+     
+      
+      <div className="text-5xl text-red-900 border-0 items-center">
+        <Link href="/week-2">
           <FaApple />
-           </Link>
-         
+        </Link>
       </div>
     </main>
   );
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -6,7 +6,6 @@ export default function Page() {
       <main>
         <h1>Shopping List</h1>
         <StudentInfo>
-
         </StudentInfo>
       </main>
     );
